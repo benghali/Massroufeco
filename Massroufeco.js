1c5c4ee0c4c84eccce2ed0e5b8b989d86b3527b7
Achref <113175845+benghali@users.noreply.github.com>
2022-10-10 18:25:37 +0100

    Add files via upload

---
+++ Massroufeco.js
@@ -0,0 +1,25 @@
+///automatic way
+function MassreoufEco(Salary){
+    var food=0.1*Salary;
+   var transportation=0.2*Salary;
+   var fuelCharges=0.2*Salary;
+   var clothes=0.05*Salary;
+    var lodging=0.35*Salary;
+    var rest=0.1*Salary;
+return "your Salary is divided on:/n"+food+"dinars on food/n"+transportation+"dinars on transportation/n"+fuelCharges+"dinars on fuel charges /n"+clothes+"dinars on clothes/n"+lodging+"dinars on lodging/n"+rest+"dinars ,the rest of your salary/n"
+}
+////////
+
+function MassroufEco(){
+
+}
+function TransportationPerMonth(salary){
+    var reuslt=0;
+    var trans=0.1;
+reuslt=salary*trans
+return reuslt+"this is your charge of transportation per month"
+}
+function foodperMonth(priceFood){
+result=0;
+
+}
